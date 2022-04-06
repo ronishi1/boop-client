da3711f0c750fc5b93b31ea8595de14cede2b0d1
Kelvin Chen <32273187+kchen6195@users.noreply.github.com>
2022-04-05 22:21:56 -0400

    StoryBoardTools has new data Board card updated

--- src/components/story_edit/BoardCard.js
+++ src/components/story_edit/BoardCard.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-const BoardCard = ({ title, cover }) => {
+const BoardCard = ({ name, image }) => {
   return (
     <div className="card text-primary-content text-center h-36 w-24 ">
       <div
         className="card-body justify-end p-2 text-white"
         style={{
-          background: `linear-gradient(180deg, hsl(0, 100%, 100%, 0), hsl(0, 0%, 0%, 0.65)),url(${cover}) no-repeat center center`,
+          background: `linear-gradient(180deg, hsl(0, 100%, 100%, 0), hsl(0, 0%, 0%, 0.65)),url(${image}) no-repeat center center`,
           backgroundSize: "cover",
         }}
       >
         <div
           className="font-medium card-title"
-          style={{ fontSize: ".5rem", lineHeight: ".5rem" }}
+          style={{ fontSize: "0.6rem", lineHeight: ".5rem" }}
         >
-          {title}
+          {name}
         </div>
       </div>
     </div>
