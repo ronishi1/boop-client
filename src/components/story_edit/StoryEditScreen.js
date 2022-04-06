5292c81013aff4c288ce74ece94607b394e2aaca
Kelvin Chen <32273187+kchen6195@users.noreply.github.com>
2022-04-05 23:59:12 -0400

    Clicking into item in storyboard tool hardcoded

--- src/components/story_edit/StoryEditScreen.js
+++ src/components/story_edit/StoryEditScreen.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import StoryBoardTool from './StoryBoardTool';
-import QuillEditor from './QuillEditor';
+import React from "react";
+import StoryBoardTool from "./StoryBoardTool";
+import QuillEditor from "./QuillEditor";
 
 const StoryEditScreen = () => {
-    // Use this for your normal storyboard testing
-    return (
-        <div class="drawer drawer-end">
-            <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content">
-            <label for="my-drawer-4" class="drawer-button btn btn-primary">
-                Open drawer
-            </label>
-            </div>
-            <StoryBoardTool></StoryBoardTool>
-        </div>
-        );
-    };
+  // Use this for your normal storyboard testing
+  return (
+    <div class="drawer drawer-end">
+      <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <label for="my-drawer-4" class="drawer-button btn btn-primary">
+          Open drawer
+        </label>
+      </div>
+      <StoryBoardTool></StoryBoardTool>
+    </div>
+  );
+};
 
-    // Need to fix sidebar Arrow
+// Need to fix sidebar Arrow
 //     return (
 //         <div class="flex">
-//             {/* <div class="flex-auto"> 
+//             {/* <div class="flex-auto">
 //                 <QuillEditor></QuillEditor>
 //             </div> */}
 //             {/* <div class="drawer"> */}
@@ -33,8 +33,8 @@ const StoryEditScreen = () => {
 
 //                 <div class="drawer-content max-w-[10%]">
 //                     <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
-//                 </div> 
-//                 <StoryBoardTool></StoryBoardTool> 
+//                 </div>
+//                 <StoryBoardTool></StoryBoardTool>
 //             </div>
 //             </div> */}
 //             {/* <div>
@@ -45,11 +45,11 @@ const StoryEditScreen = () => {
 //                     <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
 //                 <div class="drawer-content">
 //                     {/* <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label> */}
-//                 </div> 
+//                 </div>
 //                     <div class="drawer-side">
 //                         <label for="my-drawer-4" class="drawer-overlay"></label>
 //                         <ul class="menu p-4 overflow-y-auto w-96 bg-base-100 text-base-content">
-                        
+
 //                         <StoryBoardTool></StoryBoardTool>
 //                         {/* <li><a>Sidebar Item 1</a></li>
 //                         <li><a>Sidebar Item 2</a></li> */}
@@ -60,6 +60,5 @@ const StoryEditScreen = () => {
 //         </div>
 //     );
 //   }
-  
-  export default StoryEditScreen;
-  
+
+export default StoryEditScreen;
