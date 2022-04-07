02b4c934ecb2164f9bbae4314d2e062379de124d
Nelson <nelsonliang02@gmail.com>
2022-04-07 00:10:16 -0400

    Drawing for content is possible.

--- src/components/comic_edit/ComicEditScreen.js
+++ src/components/comic_edit/ComicEditScreen.js
@@ -1,10 +1,14 @@
 import React, {useState, useRef} from 'react';
 import { render } from 'react-dom';
 import { Stage, Layer, Line, Text } from 'react-konva';
+import { Html } from 'react-konva-utils';
+import IroColorPicker from "./IroColorPicker"
+import Draggable from 'react-draggable'; // The default
 
 const ComicEditScreen = () => {
   const [tool, setTool] = useState('pen');
   const [lines, setLines] = useState([]);
+  const [stroke, setStroke] = useState('#df4b26')
   const isDrawing =  useRef(false);
   const stageRef = useRef(null);
   const layerRef = useRef(null);
@@ -12,16 +16,16 @@ const ComicEditScreen = () => {
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    setLines([...lines, { tool, points: [pos.x, pos.y], stroke: stroke }]);
     console.log(lines);
-    console.log(stageRef.current.toJSON())
+    // console.log(stageRef.current.toJSON())
     // ON SAVE, take the list of lines and their attributes.
     //    Mutate to graphQL and then when important, map the lines
     //    with their attributes. 
     // Exporting to cloudinary should be done through by calling
     //  stageRef.current.toDataURL()
-    console.log(layerRef.current.children)
-    console.log(stageRef.current.toDataURL())
+    // console.log(layerRef.current.children)
+    // console.log(stageRef.current.toDataURL())
   };
 
   const handleMouseMove = (e) => {
@@ -44,6 +48,13 @@ const ComicEditScreen = () => {
     isDrawing.current = false;
   };
 
+  const onColorChange = (color) => {
+    // setStroke(color)
+    setStroke(color)
+    console.log(stroke)
+    // console.log(JSON.stringify(color.color.hexstring));
+  }
+
   return (
     <div>
       <select
@@ -63,12 +74,32 @@ const ComicEditScreen = () => {
         onMouseup={handleMouseUp}
         ref = {stageRef}
       >
+      
         <Layer ref = {layerRef}>
+        <Html
+            divProps={{
+              style: {
+                position: 'absolute',
+                top: 10,
+                left: 10,
+                zIndex: 1
+              },
+            }}
+          >
+            <Draggable handle="strong">
+            <div>
+            <strong><div>DRAG ME</div></strong>
+              <div>
+                <IroColorPicker onColorChange={onColorChange}/>
+              </div>
+            </div>
+            </Draggable>
+          </Html>
           {lines.map((line, i) => (
             <Line
               key={i}
               points={line.points}
-              stroke="#df4b26"
+              stroke={line.stroke}
               strokeWidth={5}
               tension={0.5}
               lineCap="round"
@@ -80,6 +111,8 @@ const ComicEditScreen = () => {
         </Layer>
       </Stage>
     </div>
+    
+    
   );
 };
 
