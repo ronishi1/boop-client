f09207d7ab3c7f988ca08acbc0ee1e3aa9cb73d3
Iris Zheng <iris.zheng@stonybrook.edu>
2022-04-07 02:41:13 -0400

    added rating function to content info screen

--- src/components/content_info/ChapterTableEntry.js
+++ src/components/content_info/ChapterTableEntry.js
@@ -3,7 +3,7 @@ import React 	from 'react';
 const ChapterTableEntry = ({chapter}) => {
   // https://www.figma.com/file/oP2NOFuaNPMCreFx2L7iSU/Boop-Mockups?node-id=311%3A3160
   return (
-    <div className={"flex flex-row hover:cursor-pointer hover:opacity-70 hover:bg-gray-400"}>
+    <div className={"flex flex-row hover:cursor-pointer hover:bg-gray-400/25"}>
       <p>{chapter.title}</p>
       <p className="text-right pr-4">{chapter.publication_date.getMonth()+"/"
       +chapter.publication_date.getDay()+"/"+chapter.publication_date.getFullYear()}</p>
--- src/components/content_info/ContentInfoScreen.js
+++ src/components/content_info/ContentInfoScreen.js
@@ -8,9 +8,11 @@ const ContentInfoScreen = () => {
   const [favorited, toggleFavorite] = useState(false);
   const [bookmarked, toggleBookmark] = useState(false);
   const [followed, toggleFollow] = useState(false);
+  const [rating, setRating] = useState(0);
   const link = "localhost:3000/"
   const id = "";
 
+  const ratings = [1,2,3,4,5,6,7,8,9,10];
   const data = {
       title: "Kaguya-sama Love Is War",
       author: "Aka Akasaka",
@@ -40,32 +42,24 @@ const ContentInfoScreen = () => {
       }]
   };
 
-  const favorite = async () => {
+  const handleFavorite = async () => {
     toggleFavorite(!favorited);
   }
 
-  const bookmark = async () => {
+  const handleBookmark = async () => {
     toggleBookmark(!bookmarked);
   }
 
-  const follow = async () => {
+  const handleFollow = async () => {
     toggleFollow(!followed);
   }
 
-  // sharing just copies link to clipboard
-  const share = async () => {
-    await navigator.clipboard.writeText(link+"info/"+id);
+  const handleRate = async (rating) => {
+    setRating(rating);
   }
-
-  const contentColor = () => {
-    switch(data.content_type) {
-      case "C":
-        return "comic";
-      case "S":
-        return "story";
-      default:
-        return "";
-    } 
+  // sharing just copies link to clipboard
+  const handleShare = async () => {
+    await navigator.clipboard.writeText(link+"info/"+id);
   }
 
   const duplicateChapters = () => {
@@ -100,13 +94,13 @@ const ContentInfoScreen = () => {
             <figure className="h-full">
               <img className="h-full object-contain" src={data.cover_image} alt="cover art"/>
             <div className="self-start pt-1">
-              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill={favorited ? "currentColor" : "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={favorite}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill={favorited ? "currentColor" : "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={handleFavorite}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
               </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill={bookmarked ? "currentColor" : "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={bookmark}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill={bookmarked ? "currentColor" : "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={handleBookmark}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
               </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={share}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-5 w-5 hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={handleShare}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
               </svg>
             </div>
@@ -126,28 +120,24 @@ const ContentInfoScreen = () => {
             </div>
           </div>
           <div className="card-body row-start-2 row-end-3 col-start-3 col-end-5 border-b-2 flex-row items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-6 w-6 hover:opacity-70" fill={followed ? "currentColor": "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={follow}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-6 w-6 hover:opacity-70" fill={followed ? "currentColor": "none"} viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" onClick={handleFollow}>
               <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
             </svg>
             Author: {data.author}
           </div>
           <div className="card-body row-start-2 row-end-3 col-start-5 col-end-7 border-b-2 flex-col items-center justify-end">
-            <label className="label">
-              <span className="label-text">Rating: {data.current_rating}/10</span>
-            </label>
-            <select className={"select select-bordered max-w-xs border-comic"}>
-              <option disabled selected>Rate</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-              <option>9</option>
-              <option>10</option>
-            </select>
+            Rating: {data.current_rating}/10
+            <div class="dropdown">
+              <label tabindex="0" className="select select-bordered h-8 min-h-0 w-28">
+                {rating==0 ? "Rate" : rating}
+              </label>
+              <ul tabindex="0" class="dropdown-content absolute z-10 mt-2 border-solid border-2 menu bg-base-100 w-28 rounded-box overflow-auto max-h-88">
+                {ratings.map((rating) => {
+                  return <li><a className="text-sm py-1.5 h-8 hover:bg-gray-400/25" onClick={() => {handleRate(rating)}}>{rating}</a></li>
+                })}
+                
+              </ul>
+            </div>
           </div>
           <div className="card-body row-start-3 row-end-6 col-start-3 col-end-7">
           {/* DUPLICATE CHAPTERS TESTING PURPOSES ONLY */}
