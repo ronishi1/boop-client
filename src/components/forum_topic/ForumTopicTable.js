8539e14a6549808c63e74418a9a2ae5e851a0c99
Iris Zheng <iris.zheng@stonybrook.edu>
2022-04-08 16:59:47 -0400

    Forum topic screen basic set up

--- src/components/forum_topic/ForumTopicTable.js
+++ src/components/forum_topic/ForumTopicTable.js
@@ -1,10 +1,24 @@
 import React 	from 'react';
+import ForumTopicTableEntry from './ForumTopicTableEntry';
 
-const ForumTopicTable = () => {
+const ForumTopicTable = ({posts}) => {
   // https://www.figma.com/file/oP2NOFuaNPMCreFx2L7iSU/Boop-Mockups?node-id=275%3A1703
   return (
-    <div>
-      <h1>Forum Topic Filler</h1>
+    <div className='w-full h-full overflow-y-auto bg-white'>
+      <table className="w-full table-fixed bg-white border-2">
+        <thead className='border-b-2'>
+          <tr>
+            <th className='p-4 w-2/3 text-left'>Title</th>
+            <th>Replies</th>
+            <th>Recent Posts</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post) => {
+            return <ForumTopicTableEntry post={post}/>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/components/forum_topic/ForumTopicTableEntry.js
+++ src/components/forum_topic/ForumTopicTableEntry.js
@@ -1,11 +1,48 @@
 import React 	from 'react';
 
-const ForumTopicTableEntry = () => {
+const ForumTopicTableEntry = ({post}) => {
   // https://www.figma.com/file/oP2NOFuaNPMCreFx2L7iSU/Boop-Mockups?node-id=275%3A1703
+  const months = [ "Jan", "Feb", "Mar", "April", "May", "June",
+  "July", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+  
+  const formatDate = () => {
+    let date = post.publication_date;
+    let month = months[date.getMonth()-1];
+    let pm = date.getHours() > 12;
+    let hour = date.getHours();
+    if(pm) hour -= 12;
+    if(hour == 0) hour = 12;
+    let strHour = ("0" + hour).slice(-2);
+    let strMinutes = ("0" + date.getMinutes()).slice(-2);
+    return month + " " + date.getDate() + ", " + strHour + ":" + strMinutes + (pm ? "pm" : "am");
+  }
+  
   return (
-    <div>
-      <h1>Forum Topic Table Entry Filler</h1>
-    </div>
+    <tr className='hover:bg-gray-400/25'>
+      <th className='h-16 text-left grid grid-cols-2 m-2'>
+        <div className='h-16 flex flex-row'>
+          <img className="h-full w-16 mr-2 object-contain" src={post.cover_image} alt="cover art"/>
+          <div className='flex flex-col'>
+            <div className='text-lg text-link'>{post.title}</div>
+            <div className='flex flex-row text-sm'>
+              <div>{formatDate(post.publication_date)}</div>
+              <div className='px-1'> by </div>
+              <div className='text-link'>{post.author}</div>
+            </div>
+          </div>
+        </div>
+        <div className='justify-self-end'>
+          {post.tags.map((tag) => {
+            return (
+            <div class={'badge'}>
+              {tag}
+            </div>);
+          })}
+        </div>
+      </th>
+      <th>{post.replies.length}</th>
+      <th>{post.recent_post.toDateString()}</th>
+    </tr>
   );
 }
 
