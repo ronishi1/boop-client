eb9db5bce18775a878abfab1dd69cd599f9f396f
Iris Zheng <iris.zheng@stonybrook.edu>
2022-04-18 23:06:38 -0400

    image sizing for detailed content card fix

--- src/components/home/DetailedContentCard.js
+++ src/components/home/DetailedContentCard.js
@@ -5,7 +5,7 @@ const DetailedContentCard = ({title,cover,genres,synopsis}) => {
   // https://www.figma.com/file/oP2NOFuaNPMCreFx2L7iSU/Boop-Mockups?node-id=211%3A556
   return (
     <div className="flex flex-row">
-      <img className="object-fit min-w-40 min-h-60 max-h-60 max-w-40 rounded" src={cover}/>
+      <img className="object-cover w-40 h-60 rounded" src={cover}/>
       <div className="ml-2 min-h-60 max-h-60 min-w-96 max-w-96">
         <Link to="/info">
           <div className="text-lg font-medium">{title}</div>
