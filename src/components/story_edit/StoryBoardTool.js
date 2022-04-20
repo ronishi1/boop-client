0d69e18e3ec62a758f2da04d32c69222adf3cb65
Kelvin Chen <32273187+kchen6195@users.noreply.github.com>
2022-04-19 23:57:23 -0400

    story edit screen
    put everything in storyboardtools because drawers

--- src/components/story_edit/StoryBoardTool.js
+++ src/components/story_edit/StoryBoardTool.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import BoardCard from "./BoardCard";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const StoryBoardTool = () => {
   const [item, setItem] = useState([]);
+  const [text, setText] = useState(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare ut sem ut rutrum. Nam iaculis condimentum mi non ullamcorper. Nullam iaculis metus sit amet ante condimentum vulputate. Curabitur nec gravida ipsum, non pulvinar massa. Mauris vitae quam nisi. Integer tincidunt feugiat libero quis blandit. Nulla a dolor eros. Sed fringilla ante non nunc venenatis, ut suscipit orci bibendum. In porttitor est id dolor vehicula, sit amet pharetra nisl placerat. Quisque consectetur lectus quis eleifend molestie."
+  );
+  const [seriesTitle, setSeriesTitle] = useState("One Punch Man");
+  const [chapterTitle, setChapterTitle] = useState(
+    "The Return of the S Tier Hero"
+  );
+  const [chapter, setChapter] = useState("10");
 
   function renderItem(e) {
     // console.log(e);
@@ -82,71 +92,155 @@ const StoryBoardTool = () => {
   console.log(item);
   if (item.length !== 0)
     content = (
-      <div class="drawer-side ">
-        <label for="my-drawer-4" class="drawer-overlay"></label>
-
-        <div class="p-4 overflow-y-auto w-1/4 bg-base-100 ">
-          <div onClick={handleBackButton}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              />
-            </svg>
-          </div>
-          <div>Eren Yeager</div>
-          <img src="https://pbs.twimg.com/media/FCulVSXXEAMe91s.jpg?name=small" />
+      <div class="drawer drawer-end">
+        <input id="my-drawer-4" type="checkbox" class="drawer-toggle"></input>
+        <div class="drawer-content grid grid-cols-4">
           <div>
-            Birthdate: March 30 Height: 183 cm (6'0") Weight: 63 kg (139 lbs)
-            Position: 104th Trainees Squad, Scouting Legion Graduation rank: 5th
-            Eren is Shingeki no Kyojin's protagonist. His childhood friend,
-            Mikasa, notes on numerous occasions that he acts on impulse without
-            thinking things through, and she often pulls/carries/throws him when
-            he starts fighting with others to protect him from himself. Along
-            with Mikasa, he tends to spend his free time with their mutual
-            friend, Armin. His father is a doctor (whom is well respected by
-            Hannes and potentially many more) whilst his mother is a housewife.
+            <div className="ml-4 mb-4">
+              Series Title: <strong>{seriesTitle}</strong>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="ml-4 mb-4">
+                Chapter {chapter} : <strong>{chapterTitle}</strong>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-span-2">
+            <ReactQuill theme="snow" value={text} />
+          </div>
+          <div>
+            <div className="flex justify-end">
+              <div className="btn cursor-pointer mr-4 mb-4">Save</div>
+            </div>
+            <div className="grid place-content-center justify-end h-3/4">
+              <label for="my-drawer-4" class="cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+                  />
+                </svg>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-4" class="drawer-overlay"></label>
+          <div class="p-4 overflow-y-auto w-1/4 bg-base-100 ">
+            <div>
+              <div onClick={handleBackButton}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                  />
+                </svg>
+              </div>
+              <div>Eren Yeager</div>
+              <img src="https://pbs.twimg.com/media/FCulVSXXEAMe91s.jpg?name=small" />
+              <div>
+                Birthdate: March 30 Height: 183 cm (6'0") Weight: 63 kg (139
+                lbs) Position: 104th Trainees Squad, Scouting Legion Graduation
+                rank: 5th Eren is Shingeki no Kyojin's protagonist. His
+                childhood friend, Mikasa, notes on numerous occasions that he
+                acts on impulse without thinking things through, and she often
+                pulls/carries/throws him when he starts fighting with others to
+                protect him from himself. Along with Mikasa, he tends to spend
+                his free time with their mutual friend, Armin. His father is a
+                doctor (whom is well respected by Hannes and potentially many
+                more) whilst his mother is a housewife.
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   else
     content = (
-      <div class="drawer-side ">
-        <label for="my-drawer-4" class="drawer-overlay"></label>
-
-        <div class="p-4 overflow-y-auto w-1/4 bg-base-100 ">
-          <div>Characters </div>
-          <div class="grid grid-flow-col ">
-            {characters.map((char) => (
-              <div class="inline-block px-2 py-2">
-                <BoardCard
-                  name={char.character_name}
-                  image={char.character_image}
-                  renderItem={renderItem}
-                />
+      <div class="drawer drawer-end">
+        <input id="my-drawer-4" type="checkbox" class="drawer-toggle"></input>
+        <div class="drawer-content grid grid-cols-4">
+          <div>
+            <div className="ml-4 mb-4">
+              Series Title: <strong>{seriesTitle}</strong>
+            </div>
+            <div className="flex flex-row justify-between">
+              <div className="ml-4 mb-4">
+                Chapter {chapter} : <strong>{chapterTitle}</strong>
               </div>
-            ))}
+            </div>
           </div>
-          <div>Plot Points</div>
-          <div class="grid grid-flow-col ">
-            {plotpoints.map((pp) => (
-              <div class="inline-block px-2 py-2">
-                <BoardCard
-                  name={pp.plot_point_name}
-                  image={pp.plot_point_image}
-                  renderItem={renderItem}
-                />
-              </div>
-            ))}
+          <div class="col-span-2">
+            <ReactQuill theme="snow" value={text} />
+          </div>
+          <div>
+            <div className="flex justify-end">
+              <div className="btn cursor-pointer mr-4 mb-4">Save</div>
+            </div>
+            <div className="grid place-content-center justify-end h-3/4">
+              <label for="my-drawer-4" class="cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+                  />
+                </svg>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-4" class="drawer-overlay"></label>
+          <div class="p-4 overflow-y-auto w-1/4 bg-base-100 ">
+            <div>Characters </div>
+            <div class="grid grid-flow-col ">
+              {characters.map((char) => (
+                <div class="inline-block px-2 py-2">
+                  <BoardCard
+                    name={char.character_name}
+                    image={char.character_image}
+                    renderItem={renderItem}
+                  />
+                </div>
+              ))}
+            </div>
+            <div>Plot Points</div>
+            <div class="grid grid-flow-col ">
+              {plotpoints.map((pp) => (
+                <div class="inline-block px-2 py-2">
+                  <BoardCard
+                    name={pp.plot_point_name}
+                    image={pp.plot_point_image}
+                    renderItem={renderItem}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/components/story_edit/StoryEditScreen.js
+++ src/components/story_edit/StoryEditScreen.js
@@ -1,87 +1,74 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import StoryBoardTool from "./StoryBoardTool";
 import QuillEditor from "./QuillEditor";
 import BoardAddModal from "../storyboardModals/BoardAddModal";
 import BoardEditModal from "../storyboardModals/BoardEditModal";
 
 const StoryEditScreen = () => {
-  const [seriesTitle, setSeriesTitle] = useState("One Punch Man")
-  const [chapterTitle, setChapterTitle] = useState("The Return of the S Tier Hero")
-  const [chapter, setChapter] = useState("10")
+  const [seriesTitle, setSeriesTitle] = useState("One Punch Man");
+  const [chapterTitle, setChapterTitle] = useState(
+    "The Return of the S Tier Hero"
+  );
+  const [chapter, setChapter] = useState("10");
   // Use this for your normal storyboard testing
-//   return (
-//     <div class="drawer drawer-end">
-//       <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-//       <div class="drawer-content">
-//         <label for="my-drawer-4" class="drawer-button btn btn-primary">
-//           Open drawer
-//         </label>
-//         <BoardAddModal content="Character" modalName="add-char-modal" />
-//         <BoardAddModal content="Plot Point" modalName="add-plot-modal" />
-//         <BoardEditModal content="Character" modalName="edit-char-modal" />
-//         <BoardEditModal content="Plot Point" modalName="edit-plot-modal" />
-//       </div>
-//       <StoryBoardTool></StoryBoardTool>
-//     </div>
-//   );
-// };
+  //   return (
+  //     <div class="drawer drawer-end">
+  //       <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+  //       <div class="drawer-content">
+  //         <label for="my-drawer-4" class="drawer-button btn btn-primary">
+  //           Open drawer
+  //         </label>
+  //         <BoardAddModal content="Character" modalName="add-char-modal" />
+  //         <BoardAddModal content="Plot Point" modalName="add-plot-modal" />
+  //         <BoardEditModal content="Character" modalName="edit-char-modal" />
+  //         <BoardEditModal content="Plot Point" modalName="edit-plot-modal" />
+  //       </div>
+  //       <StoryBoardTool></StoryBoardTool>
+  //     </div>
+  //   );
+  // };
 
-// Need to fix sidebar Arrow
-    return (
-        <div>
-            <div className="ml-4 mb-4">
-              Series Title: <strong>{seriesTitle}</strong>
-            </div>
-            <div className="flex flex-row justify-between">
-              <div className="ml-4 mb-4">
-                Chapter {chapter} : <strong>{chapterTitle}</strong>
-              </div>
-              
-              <div>
-                <div className="btn cursor-pointer mr-4 mb-4">Save</div>
-              </div>
-            </div>
+  // Need to fix sidebar Arrow
+  return (
+    <div>
+      <StoryBoardTool />
+    </div>
+    // {/* <div class="drawer"> */}
+    //       {/* <div class="drawer-content">
+    //               <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
+    //       </div>  */}
 
-            <div>
-                <QuillEditor></QuillEditor>
-            </div>
-        </div>
-          // {/* <div class="drawer"> */}
-          //       {/* <div class="drawer-content">
-          //               <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
-          //       </div>  */}
+    //   {/* <div class="drawer drawer-end">
+    //       <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
 
-          //   {/* <div class="drawer drawer-end">
-          //       <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
+    //       <div class="drawer-content max-w-[10%]">
+    //           <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
+    //       </div>
+    //       <StoryBoardTool></StoryBoardTool>
+    //   </div>
+    //   </div> */}
+    //   {/* <div>
+    //       <label for="my-drawer-4" class="drawer-button btn btn-primary">Arrow</label>
+    //   </div> */}
+    //     <div >
+    //         <div class="drawer drawer-end">
+    //             <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
+    //         <div class="drawer-content">
+    //             {/* <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label> */}
+    //         </div>
+    //             <div class="drawer-side">
+    //                 <label for="my-drawer-4" class="drawer-overlay"></label>
+    //                 <ul class="menu p-4 overflow-y-auto w-96 bg-base-100 text-base-content">
 
-          //       <div class="drawer-content max-w-[10%]">
-          //           <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
-          //       </div>
-          //       <StoryBoardTool></StoryBoardTool>
-          //   </div>
-          //   </div> */}
-          //   {/* <div>
-          //       <label for="my-drawer-4" class="drawer-button btn btn-primary">Arrow</label>
-          //   </div> */}
-        //     <div >
-        //         <div class="drawer drawer-end">
-        //             <input id="my-drawer-4" type="checkbox" class="drawer-toggle"/>
-        //         <div class="drawer-content">
-        //             {/* <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label> */}
-        //         </div>
-        //             <div class="drawer-side">
-        //                 <label for="my-drawer-4" class="drawer-overlay"></label>
-        //                 <ul class="menu p-4 overflow-y-auto w-96 bg-base-100 text-base-content">
-
-        //                 <StoryBoardTool></StoryBoardTool>
-        //                 {/* <li><a>Sidebar Item 1</a></li>
-        //                 <li><a>Sidebar Item 2</a></li> */}
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-    );
-  }
+    //                 <StoryBoardTool></StoryBoardTool>
+    //                 {/* <li><a>Sidebar Item 1</a></li>
+    //                 <li><a>Sidebar Item 2</a></li> */}
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+  );
+};
 
 export default StoryEditScreen;
