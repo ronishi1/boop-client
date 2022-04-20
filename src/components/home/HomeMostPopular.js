4d7aaee4723cfcf495fa07a0ed051119ecd06b4c
Iris Zheng <iris.zheng@stonybrook.edu>
2022-04-20 18:26:18 -0400

    formatting fix for home page

--- src/components/home/HomeMostPopular.js
+++ src/components/home/HomeMostPopular.js
@@ -23,6 +23,7 @@ const HomeMostPopular = () => {
 
   const popularContent = data.getPopularContent;
 
+  // maxContent cannot exceed 12 unless you want to edit the tailwind config
   const maxContent = 3;
   let splitContent = [];
   // split content into arrays of length=maxContent
@@ -51,9 +52,11 @@ const HomeMostPopular = () => {
                 </svg>
               </div>
             </div>
-            {contentChunk.map((content) => (
-              <DetailedContentCard id={content._id} title={content.series_title} cover={content.cover_image} genres={content.genres} synopsis={content.synopsis} type={content.content_type}/>
-            ))}
+            <div className={"grid grid-cols-"+maxContent}>
+              {contentChunk.map((content) => (
+                <DetailedContentCard id={content._id} title={content.series_title} cover={content.cover_image} genres={content.genres} synopsis={content.synopsis} type={content.content_type}/>
+              ))}
+            </div>
             <div className="mt-[6.5rem]">
               <div className={"btn btn-circle btn-sm "+(slide == splitContent.length-1 ? "invisible" : "")} onClick={() => {setSlide(slide+1)}}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
