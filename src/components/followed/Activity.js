fe193c1b3d40582fd473d64e7cd8205fc5b1de52
Ray Onishi <rayonishi11@gmail.com>
2022-05-05 18:58:56 -0400

    Added avatars to followed user activities

--- src/components/followed/Activity.js
+++ src/components/followed/Activity.js
@@ -1,6 +1,19 @@
 import React 	from 'react';
 import {Link} from 'react-router-dom'
+import { GET_USER_PROFILE } from '../../cache/queries';
+import { useQuery, useMutation } 	from '@apollo/client';
+
 const Activity = ({activity,username}) => {
+  let profile = {};
+  const { loading, error, data, refetch } = useQuery(GET_USER_PROFILE, {
+    variables: { username: username },
+  });
+
+  if(loading) { console.log(loading, 'loading'); }
+  if(error) { console.log(error, 'error'); }
+  if(data) {
+    profile = data.getUserProfile;
+  }
 
   const months = [ "Jan", "Feb", "Mar", "April", "May", "June",
   "July", "Aug", "Sep", "Oct", "Nov", "Dec" ];
@@ -16,63 +29,112 @@ const Activity = ({activity,username}) => {
     let strMinutes = ("0" + date.getMinutes()).slice(-2);
     return month + " " + date.getDate() + ", " + strHour + ":" + strMinutes + (pm ? "pm" : "am");
   }
+  console.log(activity);
+  console.log(username);
+  console.log(profile);
 
   if(activity.activity_type == "publish_comic_chapter"){
     return (
-      <div className='grid grid-cols-12 text-xl'>
+      <div className='grid grid-cols-12 text-lg'>
         <div className='flex flex-row col-span-9'>
           <p className='pr-1'>
-            <Link to={`/profile/${username}`}><span className="text-link">{username}</span></Link>
-            &nbsp;added new chapter
-            <Link to={`/view/${activity.chapter_ID}`}><span className="text-comic">&nbsp;{activity.chapter_name}&nbsp;</span></Link>
-             to
-            <Link to={`/info/${activity.content_ID}`}><span className="text-comic">&nbsp;{activity.content_name}</span></Link>
+            <div className="flex flex-row items-center">
+              <Link to={`/profile/${username}`}>
+                <div className="flex flex-row items-center">
+                  <img
+                    className="h-8 -mx-2 object-contain mask mask-circle"
+                    src={profile.profile_pic ? profile.profile_pic : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"} />
+                  <div className="text-link">{username}</div>
+                </div>
+              </Link>
+              <div>
+                &nbsp;added new chapter
+                <Link to={`/view/${activity.chapter_ID}`}><span className="text-comic">&nbsp;{activity.chapter_name}&nbsp;</span></Link>
+                to
+                <Link to={`/info/${activity.content_ID}`}><span className="text-comic">&nbsp;{activity.content_name}</span></Link>
+              </div>
+            </div>
           </p>
         </div>
         <p className='text-right col-span-3'>{formatDate(activity)}</p>
       </div>)
-  }
-  else if (activity.activity_type == "publish_story_chapter"){
-    return (
-      <div className='grid grid-cols-12 text-xl'>
-        <div className='flex flex-row col-span-9'>
-          <p className='pr-1'>
-            <Link to={`/profile/${username}`}><span className="text-link">{username}</span></Link>
-            &nbsp;added new chapter
-            <Link to={`/view/${activity.chapter_ID}`}><span className="text-story">&nbsp;{activity.chapter_name}&nbsp;</span></Link>
-             to
-            <Link to={`/info/${activity.content_ID}`}><span className="text-story">&nbsp;{activity.content_name}</span></Link>
-          </p>
-        </div>
-        <p className='text-right col-span-3'>{formatDate(activity)}</p>
-      </div>)
-  }
-  else if(activity.activity_type == "publish_comic"){
-    return (
-      <div className='grid grid-cols-12 text-xl'>
-        <div className='flex flex-row col-span-9'>
-          <p className='pr-1'>
-            <Link to={`/profile/${username}`}><span className="text-link">{username}</span></Link>
-            &nbsp;published a new comic
-            <Link to={`/info/${activity.content_ID}`}><span className="text-comic">&nbsp;{activity.content_name}</span></Link>
-          </p>
-        </div>
-        <p className='text-right col-span-3'>{formatDate(activity)}</p>
-      </div>)
-  }
-  else if(activity.activity_type == "publish_story"){
-    return (
-      <div className='grid grid-cols-12 text-xl'>
-        <div className='flex flex-row col-span-9'>
-          <p className='pr-1'>
-            <Link to={`/profile/${username}`}><span className="text-link">{username}</span></Link>
-            &nbsp;published a new story
-            <Link to={`/info/${activity.content_ID}`}><span className="text-story">&nbsp;{activity.content_name}</span></Link>
-          </p>
-        </div>
-        <p className='text-right col-span-3'>{formatDate(activity)}</p>
-      </div>)
-  }
-}
+    }
+    else if (activity.activity_type == "publish_story_chapter"){
+      return (
+        <div className='grid grid-cols-12 text-lg'>
+          <div className='flex flex-row col-span-9'>
+            <p className='pr-1'>
+              <div className="flex flex-row items-center">
+                <Link to={`/profile/${username}`}>
+                  <div className="flex flex-row items-center">
+                    <img
+                      className="h-8 -mx-2 object-contain mask mask-circle"
+                      src={profile.profile_pic ? profile.profile_pic : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"} />
+                    <div className="text-link">{username}</div>
+                  </div>
+                </Link>
+                <div>
+                  &nbsp;added new chapter
+                  <Link to={`/view/${activity.chapter_ID}`}><span className="text-story">&nbsp;{activity.chapter_name}&nbsp;</span></Link>
+                  to
+                  <Link to={`/info/${activity.content_ID}`}><span className="text-story">&nbsp;{activity.content_name}</span></Link>
+                </div>
+              </div>
+            </p>
+          </div>
+          <p className='text-right col-span-3'>{formatDate(activity)}</p>
+        </div>)
+      }
+      else if(activity.activity_type == "publish_comic"){
+        return (
+          <div className='grid grid-cols-12 text-lg'>
+            <div className='flex flex-row col-span-9'>
+              <p className='pr-1'>
+                <div className="flex flex-row items-center">
+                  <Link to={`/profile/${username}`}>
+                    <div className="flex flex-row items-center">
+                      <img
+                        className="h-8 -mx-2 object-contain mask mask-circle"
+                        src={profile.profile_pic ? profile.profile_pic : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"} />
+                      <div className="text-link">{username}</div>
+                    </div>
+                  </Link>
+                  <div>
+                    &nbsp;published a new comic
+                    <Link to={`/info/${activity.content_ID}`}><span className="text-comic">&nbsp;{activity.content_name}</span></Link>
+                  </div></div>
+                </p>
+              </div>
+              <p className='text-right col-span-3'>{formatDate(activity)}</p>
+            </div>)
+          }
+          else if(activity.activity_type == "publish_story"){
+            return (
+              <div className='grid grid-cols-12 text-lg'>
+                <div className='flex flex-row col-span-9'>
+                  <p className='pr-1'>
+                    <div className="flex flex-row items-center">
+                      <Link to={`/profile/${username}`}>
+                        <div className="flex flex-row items-center">
+                          <img
+                            className="h-8 -mx-2 object-contain mask mask-circle"
+                            src={profile.profile_pic ? profile.profile_pic : "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"} />
+                          <div className="text-link">{username}</div>
+                        </div>
+                        </Link>
+                        <div>
+                        &nbsp;published a new story
+                        <Link to={`/info/${activity.content_ID}`}><span className="text-story">&nbsp;{activity.content_name}</span></Link>
+                      </div>
+                    </div>
+                  </p>
+                </div>
+                <p className='text-right col-span-3'>{formatDate(activity)}</p>
+              </div>)
+            }
+            else {
+              return <div></div>
+            }
+          }
 
-export default Activity;
+          export default Activity;
