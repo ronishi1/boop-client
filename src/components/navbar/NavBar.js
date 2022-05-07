8a83ce7695fd2dffb3e33a5031619934b8aa6577
Iris Zheng <iris.zheng@stonybrook.edu>
2022-05-07 17:20:00 -0400

    navbar options overlays everything

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = ({showSidebarCallback,auth,toggleLoginCallback,toggleRegisterCall
     }
   }
   return (
-    <div className="navbar bg-base-100 drop-shadow mb-5 h-14 min-h-0">
+    <div className="navbar bg-base-100 drop-shadow mb-5 h-14 min-h-0 relative z-50">
       <div className="navbar-start">
         <label tabIndex="0" className="btn btn-ghost btn-circle h-9 w-9 min-h-0 mr-1" onClick={() => {showSidebarCallback()}}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-icon-grey" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
