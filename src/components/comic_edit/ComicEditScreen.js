064a6cc927437a6203cb7459e7dbb73d25d70da8
Ray Onishi <rayonishi11@gmail.com>
2022-05-12 23:19:52 -0400

    Fixed location of feedback to add page and save

--- src/components/comic_edit/ComicEditScreen.js
+++ src/components/comic_edit/ComicEditScreen.js
@@ -471,7 +471,7 @@ const ComicEditScreen = ({tps}) => {
       </div>
       <div className="px-8 pb-4" style={{boxShadow: "0 1px 1px 0 rgb(0 0 0 / 0.1)"}}>
         <div className="flex flex-row justify-between">
-          <div className="flex flex-row w-5/6">
+          <div className="flex flex-row">
             <Link to={`/content-management/${chapter.series_id}`}>
               <div className="cursor-pointer mr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -479,36 +479,36 @@ const ComicEditScreen = ({tps}) => {
                 </svg>
               </div>
             </Link>
-          <div className='flex flex-col w-full'>
-            <p className='text-xs'>Series Title: <strong>{chapter.series_title}</strong></p>
-            <Transition
-              show={titleError.status}
-              enter="transition-opacity duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity duration-500"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="alert alert-error py-1.5 shadow">
-                <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                  <span>{titleError.message}</span>
+            <div className='flex flex-col w-full'>
+              <p className='text-xs'>Series Title: <strong>{chapter.series_title}</strong></p>
+              <Transition
+                show={titleError.status}
+                enter="transition-opacity duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="alert alert-error py-1.5 shadow">
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>{titleError.message}</span>
+                  </div>
                 </div>
+              </Transition>
+              <div className='flex flex-row text-lg space-x-2'>
+                <p className='border-2 border-transparent'>Chapter Title:</p>
+                <input
+                  placeholder="Title your chapter"
+                  className='border-2 border-transparent hover:border-inherit focus:outline-none focus:border-inherit'
+                  value={title}
+                  onFocus={() => toggleTyping(!isTyping)}
+                  onChange={(event) => { setTitle(event.target.value);}}
+                  onBlur={handleChangeTitle}
+                />
               </div>
-            </Transition>
-            <div className='flex flex-row text-lg space-x-2'>
-              <p className='border-2 border-transparent'>Chapter Title:</p>
-              <input
-                placeholder="Title your chapter"
-                className='border-2 border-transparent hover:border-inherit focus:outline-none focus:border-inherit'
-                value={title}
-                onFocus={() => toggleTyping(!isTyping)}
-                onChange={(event) => { setTitle(event.target.value);}}
-                onBlur={handleChangeTitle}
-              />
             </div>
-          </div>
           </div>
           <Transition
             show={saveClicked}
