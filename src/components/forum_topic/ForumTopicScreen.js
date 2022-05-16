d78072f0a4022a2d492aeb2b58611b6c31802adb
Ray Onishi <rayonishi11@gmail.com>
2022-05-16 04:18:38 -0400

    Removed unnecessary sorting icon

--- src/components/forum_topic/ForumTopicScreen.js
+++ src/components/forum_topic/ForumTopicScreen.js
@@ -81,41 +81,6 @@ const ForumTopicScreen = () => {
               </div>
             </div>
           </div>
-          <div class="dropdown dropdown-end justify-self-end self-center pr-2">
-            <label tabindex="0" class="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="white"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>
-            <ul
-              tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Most Views</a>
-              </li>
-              <li>
-                <a>Most Replies</a>
-              </li>
-              <li>
-                <a>Most Recent</a>
-              </li>
-              <li>
-                <a>Oldest</a>
-              </li>
-            </ul>
-          </div>
         </div>
         <ForumTopicTable posts={topic.posts} />
       </div>
